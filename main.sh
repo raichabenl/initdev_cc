ae2f68b43d7ccbdd1be841bcbf7ccac03025fb60
raichabenl <beaicha45@gmail.com>
2018-04-05 05:16:53 +0100

    doneÉ2

--- main.sh
+++ main.sh
@@ -2,6 +2,9 @@ mkdir "projet o.p"
 
 cd "projet o.p"
 mkdir "LISCENSE"
-if [ $language ="c"];then cp -r initdev/liscenses/GPL initdev/projet o.p
-~                                                                               
-~                         
+if [ $test "language" ="c"]; and [$test "LISCENCE" ="GPL"] ( cp -r initdev/source/liscenses/GPL  initdev/projet o.p/LISCENSE)
+and (cp -r source/main.c projet o.p/main.c)
+echo  $0
+else echo "Uknown arguments, please check the help : initdev –help"                                                                               
+echo $1
+fi                         
