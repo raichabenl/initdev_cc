6d16627695d451ab7642beaafb6bd383e09daef1
raichabenl <beaicha45@gmail.com>
2018-04-05 04:25:31 +0100

    done

---
+++ main.sh
@@ -0,0 +1,7 @@
+mkdir "projet o.p"
+
+cd "projet o.p"
+mkdir "LISCENSE"
+if [ $language ="c"];then cp -r initdev/liscenses/GPL initdev/projet o.p
+~                                                                               
+~                         
